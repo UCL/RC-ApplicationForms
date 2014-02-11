9634af67ff58bd2ff71256c82794b78384291940
Ian Kirker <ian.kirker@gmail.com>
2014-02-11 14:44:57 +0000

    Fixed commas syntax, split out service reqs

--- schema.sql
+++ schema.sql
@@ -2,8 +2,8 @@ CREATE TABLE Consortia
 (
   id INTEGER AUTO_INCREMENT,
   full_name TEXT NOT NULL,
-  short_name VARCHAR(7) NOT NULL,
-  PRIMARY KEY (id),
+  short_name VARCHAR(23) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE Privileged_Users
@@ -14,7 +14,7 @@ CREATE TABLE Privileged_Users
   super_special_rainbow_pegasus_powers BOOLEAN,
   receives_emails BOOLEAN,
   email_address VARCHAR(255),
-  PRIMARY KEY (id),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE Consortium_Permissions
@@ -24,40 +24,40 @@ CREATE TABLE Consortium_Permissions
   approves_for_consortium INTEGER,
   PRIMARY KEY (id),
   FOREIGN KEY (user_id) REFERENCES Privileged_Users(id),
-  FOREIGN KEY (approves_for_consortium) REFERENCES Consortia(id),
+  FOREIGN KEY (approves_for_consortium) REFERENCES Consortia(id)
 );
 
 CREATE TABLE Event_Types
 (
   id INTEGER AUTO_INCREMENT,
   event_type TEXT,
-  PRIMARY KEY (id),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE Request_Progress
 (
   id INTEGER AUTO_INCREMENT,
   request_id INTEGER,
-  event_class_id INTEGER,
+  event_type_id INTEGER,
   acting_user INTEGER,
   object TEXT,
-  when DATETIME(),
+  update_time TIMESTAMP,
   PRIMARY KEY (id),
-  FOREIGN KEY (event_class_id) REFERENCES Event_Types(id),
+  FOREIGN KEY (event_type_id) REFERENCES Event_Types(id)
 );
 
 CREATE TABLE User_Types
 (
   id INTEGER AUTO_INCREMENT,
   user_type TEXT,
-  PRIMARY KEY (id),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE User_Experience_Levels
 (
   id INTEGER AUTO_INCREMENT,
   level_text text,
-  PRIMARY KEY (id),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE Account_Request
@@ -65,6 +65,7 @@ CREATE TABLE Account_Request
   id INTEGER AUTO_INCREMENT,
   user_id VARCHAR(7),
   user_upi VARCHAR(15),
+  supervisor_upi VARCHAR(15),
   user_type INTEGER,
   user_email_address TEXT,
   user_surname TEXT,
@@ -76,7 +77,7 @@ CREATE TABLE Account_Request
   user_experience TEXT,
   PRIMARY KEY (id),
   FOREIGN KEY (user_type) REFERENCES User_Types(id),
-  FOREIGN KEY (user_experience_dropdown) REFERENCES User_Experience_Levels(id),
+  FOREIGN KEY (user_experience_dropdown) REFERENCES User_Experience_Levels(id)
 );
 
 CREATE TABLE Project
@@ -88,9 +89,6 @@ CREATE TABLE Project
   is_funded BOOLEAN,
   pi_user_id VARCHAR(7),
   consortium_id INTEGER,
-  wants_legion BOOLEAN,
-  wants_iridis BOOLEAN,
-  wants_emerald BOOLEAN,
   wants_cfi_because TEXT,
   cfi_impact TEXT,
   cfi_usage TEXT,
@@ -106,6 +104,22 @@ CREATE TABLE Project
   collab_other_institution_person TEXT,
   PRIMARY KEY (id),
   FOREIGN KEY (request_id) REFERENCES Account_Request(id),
-  FOREIGN KEY (consortium_id) REFERENCES Consortia(id),
+  FOREIGN KEY (consortium_id) REFERENCES Consortia(id)
 );
 
+CREATE TABLE Services
+(
+  id INTEGER AUTO_INCREMENT,
+  name varchar(255),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Service_Requests
+(
+  id INTEGER AUTO_INCREMENT,
+  project_id INTEGER,
+  service_id INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (project_id) REFERENCES Projects(id),
+  FOREIGN KEY (service_id) REFERENCES Services(id)
+);
