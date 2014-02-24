c2cfe870932aa3f925985cdb4a6bd295a1f6b278
Ian Kirker <ian.kirker@gmail.com>
2014-02-24 22:49:59 +0000

    Added test non-privileged user, changed double quotes for singles which seems to make PHPStorm happier.

--- base_setup.sql
+++ base_setup.sql
@@ -1,72 +1,76 @@
 REPLACE INTO Consortia (short_name, full_name) VALUES
-    ( "Astro"             , "Astrophysics and Remote Sensing"                                           ) ,
-    ( "BioinfCompBio"     , "Bioinformatics and Computational Biology"                                  ) ,
-    ( "BuiltEnv"          , "The Bartlett - Built Environment"                                          ) ,
-    ( "Climate"           , "Climate Change and Earth Observation"                                      ) ,
-    ( "digitalhumanities" , "Digital Humanities"                                                        ) ,
-    ( "ENGFEAandCFD"      , "Engineering - Finite Element Analysis & Computational Fluid Dynamics"      ) ,
-    ( "EngDAandMD"        , "Engineering Sciences – Optimisation, Data Analysis and Molecular Dynamics" ) ,
-    ( "Economics"         , "Economics"                                                                 ) ,
-    ( "Epidemiology"      , "Epidemiology"                                                              ) ,
-    ( "gatsbyneuro"       , "The Gatsby Computational Neuroscience Unit"                                ) ,
-    ( "HEP"               , "High Energy Physics"                                                       ) ,
-    ( "ISD"               , "Information Services Division"                                             ) ,
-    ( "NGS"               , "Next Generation Sequencing"                                                ) ,
-    ( "TYCOrgPharmMat"    , "Thomas Young Centre - Organic and Pharmaceutical Materials"                ) ,
-    ( "TYCEarthMat"       , "Thomas Young Centre - Earth Materials"                                     ) ,
-    ( "TYCNano"           , "Thomas Young Centre - Nanoscience and Defects"                             ) ,
-    ( "TYCCatSurf"        , "Thomas Young Centre - Surface Science and Catalysis"                       ) ,
-    ( "Maths"             , "Mathematical Sciences"                                                     ) ,
-    ( "MedImaging"        , "Medical Imaging"                                                           ) ,
-    ( "MolQuantDynam"     , "Molecular Quantum Dynamics and Electronic Structure"                       ) ,
-    ( "NeuroSci"          , "Neuroscience"                                                              ) ,
-    ( "sochistsci"        , "Social and Historical Sciences"                                            ) ,
-    ( "SysBioMed"         , "Systems Biomedicine"                                                       ) ,
-    ( "PENDING"           , "[None of these fit my research area]"                                      )
+  ( 'Astro'             , 'Astrophysics and Remote Sensing'                                           ) ,
+  ( 'BioinfCompBio'     , 'Bioinformatics and Computational Biology'                                  ) ,
+  ( 'BuiltEnv'          , 'The Bartlett - Built Environment'                                          ) ,
+  ( 'Climate'           , 'Climate Change and Earth Observation'                                      ) ,
+  ( 'digitalhumanities' , 'Digital Humanities'                                                        ) ,
+  ( 'ENGFEAandCFD'      , 'Engineering - Finite Element Analysis & Computational Fluid Dynamics'      ) ,
+  ( 'EngDAandMD'        , 'Engineering Sciences – Optimisation, Data Analysis and Molecular Dynamics' ) ,
+  ( 'Economics'         , 'Economics'                                                                 ) ,
+  ( 'Epidemiology'      , 'Epidemiology'                                                              ) ,
+  ( 'gatsbyneuro'       , 'The Gatsby Computational Neuroscience Unit'                                ) ,
+  ( 'HEP'               , 'High Energy Physics'                                                       ) ,
+  ( 'ISD'               , 'Information Services Division'                                             ) ,
+  ( 'NGS'               , 'Next Generation Sequencing'                                                ) ,
+  ( 'TYCOrgPharmMat'    , 'Thomas Young Centre - Organic and Pharmaceutical Materials'                ) ,
+  ( 'TYCEarthMat'       , 'Thomas Young Centre - Earth Materials'                                     ) ,
+  ( 'TYCNano'           , 'Thomas Young Centre - Nanoscience and Defects'                             ) ,
+  ( 'TYCCatSurf'        , 'Thomas Young Centre - Surface Science and Catalysis'                       ) ,
+  ( 'Maths'             , 'Mathematical Sciences'                                                     ) ,
+  ( 'MedImaging'        , 'Medical Imaging'                                                           ) ,
+  ( 'MolQuantDynam'     , 'Molecular Quantum Dynamics and Electronic Structure'                       ) ,
+  ( 'NeuroSci'          , 'Neuroscience'                                                              ) ,
+  ( 'sochistsci'        , 'Social and Historical Sciences'                                            ) ,
+  ( 'SysBioMed'         , 'Systems Biomedicine'                                                       ) ,
+  ( 'PENDING'           , '[None of these fit my research area]'                                      )
 ;
 
 REPLACE INTO Services (id, name) VALUES
-    (1, 'Legion'),
-    (2, 'Iridis'),
-    (3, 'Emerald')
+  (1, 'Legion'),
+  (2, 'Iridis'),
+  (3, 'Emerald')
 ;
 
-INSERT INTO Event_Types (event_type) VALUES
-    ('submitted'),
-    ('approved'),
-    ('declined')
+REPLACE INTO Event_Types (id, event_type) VALUES
+  (1, 'submitted'),
+  (2, 'approved'),
+  (3, 'declined')
 ;
 
-INSERT INTO User_Types (user_type) VALUES
-    ('Principle Investigator'),
-    ('Non-PI Researcher'),
-    ('PhD/EngD Student'),
-    ('Masters Student (Postgraduate)'),
-    ('Masters Student (Undergraduate)'),
-    ('Other Undergraduate')
+REPLACE INTO User_Types (id, user_type) VALUES
+  (1, 'Principle Investigator'),
+  (2, 'Non-PI Researcher'),
+  (3, 'PhD/EngD Student'),
+  (4, 'Masters Student (Postgraduate)'),
+  (5, 'Masters Student (Undergraduate)'),
+  (6, 'Other Undergraduate')
 ;
 
-INSERT INTO User_Experience_Levels (level_text) VALUES
-    ("Novice with No Local Support"),
-    ("Novice with Local Support"),
-    ("Linux experience but no HPC"),
-    ("Linux and HPC experience")
+REPLACE INTO User_Experience_Levels (id,level_text) VALUES
+  (1, 'Novice with No Local Support'),
+  (2, 'Novice with Local Support'),
+  (3, 'Some Linux experience but no HPC'),
+  (4, 'Some Linux and HPC experience'),
+  (5, 'Extensive Linux experience'),
+  (6, 'Extensive Linux and HPC experience')
 ;
 
-REPLACE INTO Privileged_Users 
-    (id, username, full_name, 
-        super_special_rainbow_pegasus_powers, 
-        receives_emails, email_address) 
-    VALUES
-    (1, "ccaabaa", "Brian Alston", TRUE, TRUE, "b.alston@ucl.ac.uk"),
-    (2, "ccaabcs", "Bruno Silva", TRUE, TRUE, "b.silva@ucl.ac.uk"),
-    (3, "uccaiki", "Ian Kirker", TRUE, TRUE, "i.kirker@ucl.ac.uk"),
-    (4, "uccaoke", "Owain Kenway", TRUE, TRUE, "o.kenway@ucl.ac.uk"),
-    (5, "ccaacla", "Clare Gryce", FALSE, TRUE, "c.gryce@ucl.ac.uk")
+REPLACE INTO Privileged_Users
+(id, username, full_name,
+ super_special_rainbow_pegasus_powers,
+ receives_emails, email_address)
+VALUES
+  (0, 'ccaaxxx', 'Test Non-special user', FALSE, FALSE, 'ccaaxxx@ucl.ac.uk'),
+  (1, 'ccaabaa', 'Brian Alston', TRUE, TRUE, 'b.alston@ucl.ac.uk'),
+  (2, 'ccaabcs', 'Bruno Silva', TRUE, TRUE, 'b.silva@ucl.ac.uk'),
+  (3, 'uccaiki', 'Ian Kirker', TRUE, TRUE, 'i.kirker@ucl.ac.uk'),
+  (4, 'uccaoke', 'Owain Kenway', TRUE, TRUE, 'o.kenway@ucl.ac.uk'),
+  (5, 'ccaacla', 'Clare Gryce', FALSE, TRUE, 'c.gryce@ucl.ac.uk')
 ;
 
-#INSERT INTO Consortium_Permissions (privileged_user_id, approves_for_consortium) VALUES
-#    (5, 22) -- Clare for PENDING
-#;
+SELECT id FROM Consortia WHERE short_name = 'PENDING' INTO @temp;
+INSERT INTO Consortium_Permissions (privileged_user_id, approves_for_consortium) VALUES
+  (5, @pending)
+;
 
 
