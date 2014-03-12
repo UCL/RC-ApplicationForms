3af523230c1c26b4ec5530d98da6e609bc15f670
Ian Kirker <ian.kirker@gmail.com>
2014-03-12 09:41:11 +0000

    Enhanced auth shim to allow arbitrary usernames

--- auth_user_shim.php
+++ auth_user_shim.php
@@ -2,7 +2,10 @@
 if ( (FALSE == array_key_exists("PHP_AUTH_USER", $_SERVER)) ||
      (is_null($_SERVER["PHP_AUTH_USER"])) )
 {
-    $current_username = "ccaaxxx";
+    if (is_null($_REQUEST['user'])) {
+        $current_username = "ccaaxxx";
+    } else {
+        $current_username = $_REQUEST['user'];
 } else {
     $current_username = $_SERVER['PHP_AUTH_USER'];
 }
