dd0cb7c48f62925f684f9d0af2e234dccbde1a4a
Ian Kirker <ian.kirker@gmail.com>
2014-03-12 13:10:26 +0000

    made more sturdy -- don't want exceptions thrown

--- auth_user_shim.php
+++ auth_user_shim.php
@@ -2,10 +2,10 @@
 if ( (FALSE == array_key_exists("PHP_AUTH_USER", $_SERVER)) ||
      (is_null($_SERVER["PHP_AUTH_USER"])) )
 {
-    if (is_null($_REQUEST['user'])) {
-        $current_username = "ccaaxxx";
-    } else {
+    if (array_key_exists("user", $_REQUEST)) {
         $current_username = $_REQUEST['user'];
+    } else {
+        $current_username = "ccaaxxx";
     }
 } else {
     $current_username = $_SERVER['PHP_AUTH_USER'];
