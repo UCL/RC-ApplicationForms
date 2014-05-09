ba4c98dbbf3ee63d0a29d0cdbdb8c84e33b0ca3a
Ian Kirker <ian.kirker@gmail.com>
2014-05-09 17:58:05 +0100

    Initial implementation

--- includes/ProjectRequestStatus.php
+++ includes/ProjectRequestStatus.php
@@ -7,5 +7,52 @@
  */
 
 class ProjectRequestStatus {
+    private $id;
+    private $project_request_id;
+    private $status_type_id;
+    private $acting_user;
+    private $comment;
+    private $update_time;
 
-} 
+    /** @var SQLActor actor */
+    protected $actor;
+
+    public function __construct($actor=NULL) {
+        if ($actor == NULL) {
+            $this->actor = new SQLActor();
+            $this->actor->connect();
+        } else {
+            $this->actor = $actor;
+        }
+    }
+
+    public static function from_db($request_id) {
+        $instance = new self();
+        $project_request_status_array = $instance->actor->get_project_request_status($request_id);
+        $instance->fill_from_array($project_request_status_array);
+        return $instance;
+    }
+
+    public function fill_from_array($project_request_status_array) {
+        foreach (array_keys($project_request_status_array) as $key) {
+            $this->$key = $project_request_status_array[$key];
+        }
+    }
+
+    public function get_status_text() {
+        return $this->actor->get_status_type($this->status_type_id);
+    }
+
+    public function get_acting_user() {
+        return $this->acting_user;
+    }
+
+    public function get_comment() {
+        return $this->comment;
+    }
+
+    public function get_update_time() {
+        return $this->update_time;
+    }
+
+}
