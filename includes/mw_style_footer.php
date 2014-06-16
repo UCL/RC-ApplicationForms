75460c6ca0bb5ef90fc071f513f54e97ff0d770d
Ian Kirker <ian.kirker@gmail.com>
2014-06-16 16:12:36 +0100

    First paste of header and footer to match the wiki's style

---
+++ includes/mw_style_footer.php
@@ -0,0 +1,64 @@
+</div>                           <!-- end main slot where content gets rendered--> 
+                        
+                        </div>
+                        <!--End center content area-->
+                        <!-- start content footer information -->
+                        <!-- end content footer information -->
+
+                     </div>
+                     <div id="ie_clearing">&nbsp;</div>
+                     <!-- End: IE Column Clearing -->
+                  </div>
+                  <!-- end: #col3 -->
+               </div>
+            </div>
+            <!-- end: #main -->
+            <!-- begin: #footer -->
+            <!-- =============== FOOTER =============== -->
+
+
+            <div id="footer"> 
+               <ul>
+                  <li><a href="http://www.ucl.ac.uk/disclaimer">Disclaimer</a></li>
+                  <li><a href="http://www.ucl.ac.uk/foi">Freedom of Information</a></li>
+                  <li><a href="http://www.ucl.ac.uk/accessibility">Accessibility</a></li>
+                  <li><a href="http://www.ucl.ac.uk/privacy">Privacy</a></li>
+                  <li><a href="http://www.ucl.ac.uk/cookies">Cookies</a></li>
+                  <li><a href="http://www.ucl.ac.uk/advanced-search">Advanced Search</a></li>
+                  <li><a href="http://www.ucl.ac.uk/contact-list/">Contact Us</a></li>
+              </ul>
+  
+              <div>
+                 <address class="vcard">
+                    <span class="adr">
+                       University College London - Gower Street - London - WC1E 6BT
+                    </span>
+                    <span class="tel">
+                       <span class="type">Tel</span>:
+                       <span class="value">+44 (0)20 7679 2000</span>
+                    </span>
+                 </address>
+                 <p>&#169; UCL 1999&#8211;2014</p>
+              </div>
+           </div>
+
+ 
+           <!-- end #footer -->
+        </div>
+        <!-- Graphic Border - Begin Part 2 -->
+        <div id="border-bottom">
+           <div id="edge-bl"> </div>
+           <div id="edge-br"> </div>
+        </div>
+        <!-- Graphic Border - End Part 2 -->
+     </div>
+      
+
+
+
+<!-- scripts and debugging information -->
+
+
+</body>
+</html>
+
---
+++ includes/mw_style_header.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en"
+      xml:lang="en">
+   <!DOCTYPE html>
+<html lang="en-GB" dir="ltr" class="client-nojs">
+<head>
+<title>Research Computing Platform Services Accounts</title>
+<meta charset="UTF-8" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="search" type="application/opensearchdescription+xml" href="/mediawiki119/opensearch_desc.php" title="Research Computing Platform Services Wiki (en-gb)" />
+<link rel="stylesheet" href="//wiki.rc.ucl.ac.uk/mediawiki119/load.php?debug=false&amp;lang=en-gb&amp;modules=mediawiki.legacy.commonPrint%2Cshared&amp;only=styles&amp;skin=uclrc&amp;*" />
+<link rel="stylesheet" href="/mediawiki119/skins/uclrc/main.css?303" media="screen" /><meta name="ResourceLoaderDynamicStyles" content="" />
+<link rel="stylesheet" href="//wiki.rc.ucl.ac.uk/mediawiki119/load.php?debug=false&amp;lang=en-gb&amp;modules=site&amp;only=styles&amp;skin=uclrc&amp;*" />
+<style>a:lang(ar),a:lang(ckb),a:lang(fa),a:lang(kk-arab),a:lang(mzn),a:lang(ps),a:lang(ur){text-decoration:none}a.new,#quickbar a.new{color:#ba0000}
+
+/* cache key: wikidb:resourceloader:filter:minify-css:7:c88e2bcd56513749bec09a7e29cb3ffa */
+</style>
+
+</head>
+<body class="mediawiki ltr sitedir-ltr ns-0 ns-subject page-Main_Page skin-uclrc action-view">
+   <head>
+      <title>Research Computing Platforms Services</title>
+      <link rel="shortcut icon" href="//www.ucl.ac.uk/departments/++resource++images/favicon2.ico" />
+         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+         <meta name="contact.name" content="UCL Research Computing" />
+         <meta name="contact.email" content="rc-support@ucl.ac.uk" />
+
+         <!--MediaWiki Common Stylesheets-->
+                  <!--End MediaWiki Common Stylesheets-->
+
+         <!--Start General Layout Styles-->
+            <!--Start UCL default layout 2 column styles-->
+            <link href="//www.ucl.ac.uk/departments/++resource++css/ucl_default_2col_layout.css"
+                  rel="stylesheet" type="text/css" />
+            <!--[if lte IE 8]><link rel="stylesheet" type="text/css" href="//www.ucl.ac.uk/departments/++resource++patches/patch_ucl_default_2col_layout.css" /><![endif]-->
+            <!--End UCL default layout 2 column styles-->
+            
+            <!--Corporate identity styling below-->
+            <link href="//www.ucl.ac.uk/departments/++resource++css/screen/corp-identity-light-blue.css"
+                  rel="stylesheet" type="text/css" />
+            <!--Corporate identity styling above-->
+         <!--End General Layout Styles-->
+
+         <!--Start Print CSS-->
+         <link href="//www.ucl.ac.uk/departments/++resource++css/print/print.css"
+               media="print" rel="stylesheet"
+               type="text/css" />
+         <!--Set IE to be print smaller width, so that it doesn't crop content-->
+         <!--[if lte IE 8]>
+             <style type="text/css">
+                 @media print {body, div.page, #main{width:600px;}}
+             </style>
+         <![endif]-->
+         <!--End Print CSS-->
+
+         <!--Start JQuery Javascript-->
+            <script src="//ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+            <script>window.jQuery || document.write('<script src="//www.ucl.ac.uk/news/++resource++default_scripts/jquery-1.3.2.min.js"><\/script>')</script>
+         <!--End JQuery Javascript-->
+        
+    
+
+         <!--Start UCL Javascript-->
+            <!--<script type="text/javascript" tal:attributes="src string:${resourcebase}/++resource++default_scripts/ucl.min.js" src="/js/ucl/ucl.min.js"></script>-->
+         <!--End UCL Javascript-->
+
+         <!--Start JQuery Core User Interface Javascript-->
+            <script type="text/javascript"
+                    src="//www.ucl.ac.uk/departments/++resource++default_scripts/ui.core.min.js"></script>
+         <!--End JQuery Core User Interface Javascript-->
+
+         <!--Start JQuery Font Resizer Javascript-->
+            <script type="text/javascript"
+                    src="//www.ucl.ac.uk/departments/++resource++default_scripts/fontsizer.jquery.js"></script>
+         <!--End JQuery Font Resizer Javascript-->
+
+         <!--Start JQuery cookie Javascript-->
+            <script type="text/javascript"
+                    src="//www.ucl.ac.uk/news/++resource++default_scripts/jquery.cookie.min.js"></script>
+         <!--End JQuery cookie Javascript-->
+
+
+
+
+         <script type="text/javascript"
+                 src="//www.ucl.ac.uk/departments/++resource++default_scripts/jquery.doc.ready.js"></script>
+
+         <!--Steal Silva Style Sheets-->
+         <!-- <link rel="stylesheet" type="text/css" href="//www.ucl.ac.uk/isd/staff/research_services/local-styles.css"> -->
+         <!-- Add a margin back to the left column. -->
+         <style>
+            #left-silva-content{ margin-top: 1em; }
+         </style>
+         <!-- This page is originally templated on the news sites but then I took some bits from the faculties website. -->
+         <link rel="stylesheet" type="text/css"
+               href="//www.ucl.ac.uk/departments/local-styles.css" />
+
+         <!-- Stylesheet for high contrast style switching -->
+         <link rel="stylesheet" type="text/css"
+               id="contrastCSS"
+               href="//www.ucl.ac.uk/departments/++resource++css/screen/ucl_normal_view.css" />
+         <!--end Silva Style Sheets-->
+         
+         <!-- styling to actually make links different to h5 elements.  -->
+         <style>
+            a:link, a:visited { color: #134C92; }
+         </style>
+
+    </head>
+    <body>
+                     <div class="page_margins">
+          <!-- Graphic Border - Begin Part 1 -->
+             <div id="border-top">
+                <div id="edge-tl"> </div>
+                <div id="edge-tr"> </div>
+             </div>
+             <!-- Graphic Border - End Part 1 -->
+             <div class="page">
+                <div id="printLogo">
+                   <img src="//www.ucl.ac.uk/departments/++resource++images/printlogo.gif"
+                        alt="UCL logo" />
+                </div>
+                        
+                <div id="header" class="tb-dark_grey">
+                <div id="topnav">
+                   <!-- start: skip link navigation -->
+                   <a class="skip" href="#navigation" title="skip link">skip to navigation</a><span class="hideme">.</span>
+                   <a class="skip" href="#content" title="skip link">skip to content</a><span class="hideme">.</span>
+                   <!-- end: skip link navigation -->
+                </div>
+                <h1 class="section_header_white" style="margin-left: 1em;">
+        Research Computing Platform Services Wiki <br /><br /><span style="font-style: italic;"> </span>
+                </h1>
+                <span class="section_subheader_white"></span>
+                <div id="logo_holder"> 
+                   <a href="/"><img src="//www.ucl.ac.uk/departments/++resource++images/corp-identity-light-blue.gif"
+                                    alt="UCL Home" /></a>
+                </div> 
+             </div>
+
+             <!-- Begin breadcrumbs row -->
+             <div id="nav" class="noprint">
+                <div class="hlist" id="corp-identity-light-blue">
+                <!--Start Search box-->
+                <div id="search">
+                   <!--<label for="searchInput">Search</label>-->
+                   <form action="/wiki/Special:Search" id="googlesearch">
+                            <input placeholder="Search Wiki" id="query" name="search" type="text" style="width: 100px;" />
+                            <!--  This is the Go button: -->
+                            <input type='submit' name="go" class="submit" value="Go" /> 
+                            <!-- And this is the search button -->
+                            <input type='submit' name="fulltext" id="mw-searchButton" class="submit" value="Search" />
+                   </form>
+                </div>
+                <!--End search box-->
+                <ul width="50%">
+                   <li>
+                      <a href="//www.ucl.ac.uk/">UCL Home</a>
+                   </li>
+                   <li>
+                      <a href="//www.ucl.ac.uk/research-computing/">RC</a>
+                   </li>
+                   <li>
+                      <a href="/wiki/Main_Page" >Wiki</a>
+                   </li>
+                   <li>
+                      <a href="">Research Computing Platforms Services</a> 
+                      </li>
+        </ul>
+                <!--<div class="hlist" style="vertical-align:text-top; color: #ff0000;"> -->
+                   <ul style="text-align: right;">
+                                <li id="pt-anonuserpage"><a href="/wiki/User:128.40.14.83"Array class="new">128.40.14.83</a></li>
+                                <li id="pt-anontalk"><a href="/wiki/User_talk:128.40.14.83"Array class="new">Talk for this IP address</a></li>
+                                <li id="pt-anonlogin"><a href="/mediawiki119/index.php?title=Special:UserLogin&amp;returnto=Main+Page"Array>Log in</a></li>
+                </ul>
+                <!-- </div> -->
+             </div>
+          </div>
+
+          <!-- begin: main content area #main -->
+                       <div id="main">
+             <div>
+                <!-- begin: #col1 - first float column -->
+                <div id="col1">
+                   <div id="col1_content" class="clearfix">
+                      <div id="col_top_left" class="col_top left_coltop">
+                         <!-- logo goes here? -->
+                      </div>
+                      <a id="navigation" name="navigation"></a>
+
+                      <!-- Left-hand nav bar -->
+                      <!--  This isn't actually silva content, I just wanted to not have to modify the stylesheet. :/ -->
+                      <div id="left-silva-content"
+                           class="leftcontainer">
+
+                         <h3 class="heading">Research Computing Platform Services</h3>
+                         <ul class="disc">
+
+                                                  <div class='portlet' id='p-x'>
+                            <h5></h5>
+                            <div class='pBody'>
+                               <ul>
+                                                                 <li id="n-mainpage-description"><a href="/wiki/Main_Page"Array>Wiki Main Page</a></li>
+                                                                 <li><a href="/accounts/apply.php"Array>Wiki Main Page</a></li>
+                                                                 <li><a href="/accounts/view_user_profile.php"Array>Users</a></li>
+                                                                 <li><a href="/accounts/view_project_request.php"Array>Projects</a></li>
+                                                                 <li><a href="/accounts/view_application.php"Array>Applications</a></li>
+                                                              </ul>
+                            </div>
+                         </div>
+                         
+                         </ul>
+                         <!--
+                         <h3 class="heading">Other links</h3>
+                         <ul class="disc">
+                            <li></li> 
+                         </ul> -->
+                                                 <!-- Begin Sidebar Page Toolbox -->
+                                                        <!-- End Page Toolbox -->
+
+                     </div>
+
+                     <!-- End left bar stuff -->
+                  </div>
+               </div>
+               <!-- end: #col1 -->
+
+               <!-- begin: #col3 static column -->
+               <div id="col3">
+                  <div id="col3_content" class="clearfix">
+                     <a id="content" name="content"></a>
+                     <!-- skiplink anchor: Content -->
+                     <div id="toptabs-container" class="top-tabs">
+                                                
+                                                                     </div>
+                     <!--Start center content area-->      
+                     <!-- start main slot where content gets rendered--> 
+                     <div class="newsitem">
+                        <div style="position:relative;">  
+                                       <h2 class="heading newsitemheading">
+                  <?php echo $page_title; ?>            </h2>
+                        </div>
+                        <div class="newsiteminfo">
+                           <p><strong class="datepublished-article"></strong></p>
+                        </div>
+                        
+                        <!-- This is style info for the main content region. It's here so that it can be easily modified by PHP. -->
+                        <!-- In the long run, it might be better to make a logged in and not logged in stylesheet separately. -->
+                        <!-- (And a separate printing one, I guess?) -->
+                        <style>
+
+                           /* Heading styles */ 
+                           #mw-content-text h1 { width: 100%; border-bottom: 1px solid #E0E0E0; }
+                           #mw-content-text h2 { width: 100%; border-bottom: 1px solid #E0E0E0; }
+                           h1 span.mw-headline { font-size: 13pt; font-weight: bold; } 
+                           h2 span.mw-headline { font-size: 12pt; font-weight: bold; } 
+                           h3 span.mw-headline { font-size: 11pt; font-weight: bold; } 
+                           h5 span.mw-headline { font-size: 11pt; font-weight: bold; font-style: italic; } 
+                           h4 span.mw-headline { font-size: 11pt; font-weight: bold; font-style: italic; color: #999999 } 
+                           h6 span.mw-headline { font-size: 10pt; font-style: italic; color: #999999; } 
+                           a.new { color: #AAAAFF; text-decoration: none; border-bottom: 1px #AAAAFF dotted; } /* Links to pages that don't yet exist. */
+                           
+
+                           /* Diff page styling */
+                           *.diff { background-color: #F0F0F0; }
+                           *.diff-addedline { background-color: #CDFAC8; color: #BF0000; }
+                           *.diff-deletedline { background-color: #FFD4D4; }
+                           *.diff-context { background-color: #E0E0E0; }
+                           #mw-diff-otitle4 { text-align: center; } /* Older revision link */
+                           #mw-diff-ntitle4 { text-align: center; } /* Newer revision link */
+                           h2.diff-currentversion-title { font-size: 12pt; font-weight: bold; }
+
+                           /* Notice boxes */
+                           /*
+                           .noticebox-info          { border: 1px solid black; background-color: #EEEEFF; width:75%; }
+                           .noticebox-outage        { border: 1px solid black; background-color: #FFEEEE; width:75%; }
+                           .noticebox-warning       { border: 1px solid black; background-color: #FFE0F0; width:75%; }
+                           .noticebox-info-small    { border: 1px solid black; background-color: #EEEEFF; width:300px; float: right; }
+                           .noticebox-outage-small  { border: 1px solid black; background-color: #FFEEEE; width:300px; float: right; }
+                           .noticebox-warning-small { border: 1px solid black; background-color: #FFE0F0; width:300px; float: right; }
+                           */
+                           /* Contents box */
+               /* #toc { background-color: #eeeeff; padding: 0.1em; margin: 1em; border: 1px solid black; } */
+               #toc { background-color: #f7f7f7; padding: 0.1em; margin: 1em 0em 1em 0em; border: 1px solid #cccccc; } 
+                           #toctitle h2 { font-size:12pt; font-weight: bold; }
+                           #toc ul { margin: 0 0 0 0; }
+                           #lastmod { font-style: italic; text-align: right; list-style-type: none; }
+                           #viewcount { font-style: italic; text-align: right; list-style-type: none; }
+                                                         /* Note! This does not remove the link from the page, 
+                               *  so non-logged in users should be set to not be able 
+                               *  to edit pages regardless. 
+                               * Removing the edit links is just for cleanliness.
+                               */
+                                 span.editsection { visible=none; }
+                              
+                        </style>
+
+                        <div class="mwcontent">
+                           
